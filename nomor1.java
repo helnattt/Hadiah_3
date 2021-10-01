21c37a390cf984f798f3afb463f6e22acfda2273
helennatalia <84366306+helnattt@users.noreply.github.com>
2021-10-01 23:45:34 +0800

    Add files via upload

---
+++ nomor1.java
@@ -0,0 +1,12 @@
+class nomor1 {
+	public static void main (String[]args) {
+			short aa =5;
+			int bb =6;
+			float cc =0.5f;
+			
+			System.out.println("aa + bb      =" + (aa+bb));
+			System.out.println("aa * bb * cc =" + (aa*bb*cc));
+			System.out.println("bb / cc      =" + (bb/cc));
+	
+	}
+}
---
+++ nomor2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+class nomor2 {
+	public static void main(String arfs[]){
+		DataInputStream in = new DataInputStream(System.in);
+	
+		String nama="";
+		String alamat="";
+		String umur="";
+	
+		
+		try {
+			System.out.print("Nama    :");
+			nama= in.readLine();
+		}
+			catch(IOException e)
+			{System.out.println(e);}
+		try {
+			System.out.print("Alamat  :");
+			alamat= in.readLine();
+		}
+		catch(IOException e)
+			{System.out.println(e);}
+			
+			try {
+			System.out.print("Umur    :");
+			umur= in.readLine();
+		}
+		catch(IOException e)
+			{System.out.println(e);}
+			
+			
+		
+		
+	
+	System.out.println("Saudara    : " + (nama));
+	System.out.println("Tinggal di : " + (alamat));
+	System.out.println ("Berumur    : " + (umur) + " tahun");
+	
+	}
+	}
